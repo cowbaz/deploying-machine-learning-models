19094669eb85eb8a2ccdfb201e16f7cf4bb6e9d7
cowbaz <cowbaz@gmail.com>
2023-06-13 18:01:32 +0100

    Update the circleci yaml file for NN model

--- scripts/fetch_kaggle_large_dataset.sh
+++ scripts/fetch_kaggle_large_dataset.sh
@@ -3,7 +3,7 @@
 TRAINING_DATA_URL="vbookshelf/v2-plant-seedlings-dataset"
 NOW=$(date)
 
-kaggle datasets download -d $TRAINING_DATA_URL -p packages/neural_network_model/neural_network_model/datasets/ && \
+kaggle datasets download -d $TRAINING_DATA_URL -p  packages/neural_network_model/neural_network_model/datasets/ && \
 unzip packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset.zip -d packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset && \
 echo $TRAINING_DATA_URL 'retrieved on:' $NOW > packages/neural_network_model/neural_network_model/datasets/training_data_reference.txt && \
 mkdir -p "./packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/Shepherds Purse"  && \
